68a98507480d869fe946f066f1f327aff1939865
Ruzo <63430135+Rushell-max@users.noreply.github.com>
2021-06-28 19:19:55 -0500

    Delete Scripts_EventCorps directory

--- Scripts_EventCorps/Table_categories.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `categories` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `CategoryName` VARCHAR(500) NOT NULL,
-  `CategoryDescription` TEXT NOT NULL, 
-PRIMARY KEY (`ID`));
--- Scripts_EventCorps/Table_organizers.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `organizers` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `OrganizerName` VARCHAR(500) NOT NULL,
-  `OrganizerDescription` TEXT NOT NULL, 
-PRIMARY KEY (`ID`));
