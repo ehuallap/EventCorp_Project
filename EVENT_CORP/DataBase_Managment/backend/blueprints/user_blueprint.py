d97160c9728073e739a9090e76e8786a68beeac8
ehuallap <83515313+ehuallap@users.noreply.github.com>
2021-06-30 10:30:53 -0500

    Update user_blueprint.py

--- EVENT_CORP/DataBase_Managment/backend/blueprints/user_blueprint.py
+++ EVENT_CORP/DataBase_Managment/backend/blueprints/user_blueprint.py
@@ -11,7 +11,7 @@ user_blueprint = Blueprint('user_blueprint', __name__)
 
 model = TaskUsers()
 
-@task_blueprint.route('/user/create_user', methods=['POST'])
+@user_blueprint.route('/user/create_user', methods=['POST'])
 @cross_origin()
 def create_user():
     content = model.create_user(request.json['last_name'], request.json['name'],
@@ -19,17 +19,17 @@ def create_user():
     return jsonify(content)
 
 
-@task_blueprint.route('/user/get_user', methods=['POST'])
+@user_blueprint.route('/user/get_user', methods=['POST'])
 @cross_origin()
 def get_user():
     return jsonify(model.get_user(int(request.json['id'])))
 
-@task_blueprint.route('/user/get_users', methods=['POST'])
+@user_blueprint.route('/user/get_users', methods=['POST'])
 @cross_origin()
 def get_users():
     return jsonify(model.get_users())
 
-@task_blueprint.route('/user/delete_user', methods=['POST'])
+@user_blueprint.route('/user/delete_user', methods=['POST'])
 @cross_origin()
 def delete_user():
     return jsonify(model.delete_user(int(request.json['id'])))
