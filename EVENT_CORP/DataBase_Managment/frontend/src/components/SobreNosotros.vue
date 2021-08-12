3f48b2f68abd7b27533cc1626a6f61ab0fde09f7
RickyMaru <83198652+RickyMaru@users.noreply.github.com>
2021-08-11 22:51:53 -0500

    Update SobreNosotros.vue

--- EVENT_CORP/DataBase_Managment/frontend/src/components/SobreNosotros.vue
+++ EVENT_CORP/DataBase_Managment/frontend/src/components/SobreNosotros.vue
@@ -2,12 +2,18 @@
             <div class="content-center">
                 <h1>Sobre Nosotros</h1>
                 <h5>Somos un grupo de estudiantes de la Universidad Nacional <br> de San Agustín de Arequipa.</h5>
+                <div class="content-center">
+                  <img src="../assets/pabellon.png" class="Icon3D" alt="Logo">
+                </div>
 
                 <h1>Nuestra Misión</h1>
                 <h5>Alguna vez no te enteraste de alguna actividad o evento cerca de ti?
                 <br>O alguna vez se te paso uno por olvido?
                 <br>O tal vez nunca supiste como enterarte de los eventos cerca de ti?
-                <br>Gracias al altísimo que estamos aquí. :D</h5>
+                <br>No te preocupes, estamos aqui para ayudarte :D</h5>
+                <div class="content-center">
+                  <img src="../assets/organizacion.jpg" class="Icon3D" alt="Logo">
+                </div>
                 <br><hr><br>
                 <div class="row">
                     <div class="col-md-4 col align-self-center">
@@ -16,7 +22,7 @@
                     <div class="col-md-8">
                         <div class="content-center">
                             <div class="content-center">
-                                <img src="../assets/Logo.png" class="Icon3D" alt="Logo">
+                                <img src="../assets/party2.jpg" class="Icon3D" alt="Logo">
                             </div>
                         </div>
                     </div>
