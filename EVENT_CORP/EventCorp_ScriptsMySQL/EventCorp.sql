c5e02d599823220c99169b36603fdbd48bb9366c
Ruzo <63430135+Rushell-max@users.noreply.github.com>
2021-08-12 18:04:01 -0500

    Update DataBase

--- EVENT_CORP/EventCorp_ScriptsMySQL/EventCorp.sql
+++ EVENT_CORP/EventCorp_ScriptsMySQL/EventCorp.sql
@@ -2,27 +2,6 @@ CREATE DATABASE EventCorp;
 
 USE EventCorp;
 
-CREATE TABLE IF NOT EXISTS Events(
-	EventID INT AUTO_INCREMENT NOT NULL,
-	EventTitle VARCHAR(50) NOT NULL,
-	EventDescription TEXT,
-	EventDateStart VARCHAR(30) NOT NULL,
-	EventDateEnd VARCHAR(30) NOT NULL,
-	EventSubscribers INT NOT NULL,
-	PRIMARY KEY(EventID)
-);
-ALTER TABLE Events AUTO_INCREMENT = 1;
-
-CREATE TABLE IF NOT EXISTS Users(
-	UserID INT AUTO_INCREMENT NOT NULL,
-	UserLastName VARCHAR(500) NOT NULL,
-	UserName VARCHAR(500) NOT NULL,
-	UserEmail VARCHAR(300) NOT NULL,
-	UserEventsJoined INT NOT NULL,
-	PRIMARY KEY (UserID)
-);
-ALTER TABLE Users AUTO_INCREMENT = 1;
-
 CREATE TABLE IF NOT EXISTS Organizers(
 	OrganizerID INT AUTO_INCREMENT NOT NULL,
 	OrganizerName VARCHAR(500) NOT NULL,
@@ -39,42 +18,40 @@ CREATE TABLE IF NOT EXISTS Categories(
 );
 ALTER TABLE Categories AUTO_INCREMENT = 1;
 
-CREATE TABLE IF NOT EXISTS Organizers_Events(
-	Events_EventID INT NOT NULL,
-	Organizers_OrganizerID INT NOT NULL,
-	PRIMARY KEY(Events_EventID, Organizers_OrganizerID),
-	CONSTRAINT fkOrganizer_Events_Organizer
-	FOREIGN KEY(Organizers_OrganizerID)
-	REFERENCES Organizers(OrganizerID),
-	CONSTRAINT fkOrganizer_Organizer_Events
-	FOREIGN KEY(Events_EventID)
-	REFERENCES Events(EventID)
+CREATE TABLE IF NOT EXISTS Events(
+	EventID INT AUTO_INCREMENT NOT NULL,
+	EventTitle VARCHAR(50) NOT NULL,
+	EventDescription TEXT,
+    EventDateTimeStart DATETIME,
+    EventDateTimeEnd DATETIME,
+	EventSubscribers INT NOT NULL,
+    EventImage BLOB,
+    OrganizerID int,
+    FOREIGN KEY(OrganizerID) REFERENCES Organizers(OrganizerID),
+    CategoryID int,
+    FOREIGN KEY(CategoryID)REFERENCES Categories(CategoryID),
+	PRIMARY KEY(EventID)
 );
-ALTER TABLE Organizers_Events AUTO_INCREMENT = 1;
+ALTER TABLE Events AUTO_INCREMENT = 1;
 
-CREATE TABLE IF NOT EXISTS Categories_Events(
-	Events_EventID INT NOT NULL,
-	Categories_CategoryID INT NOT NULL,
-	PRIMARY KEY(Events_EventID, Categories_CategoryID),
-	CONSTRAINT fkCategory_Events_Category
-	FOREIGN KEY(Categories_CategoryID)
-	REFERENCES Categories(CategoryID),
-	CONSTRAINT fkCategory_Category_Events
-	FOREIGN KEY(Events_EventID)
-	REFERENCES Events(EventID)
+CREATE TABLE IF NOT EXISTS Users(
+	UserID INT AUTO_INCREMENT NOT NULL,
+	UserLastName VARCHAR(500) NOT NULL,
+	UserFirstName VARCHAR(500) NOT NULL,
+	UserEmail VARCHAR(300) NOT NULL,
+	UserEventsJoined INT NOT NULL,
+    UserPassword VARCHAR(30),
+	PRIMARY KEY (UserID)
 );
-ALTER TABLE Categories_Events AUTO_INCREMENT = 1;
+ALTER TABLE Users AUTO_INCREMENT = 1;
+
 
 CREATE TABLE IF NOT EXISTS Users_Events(
 	Events_EventID INT NOT NULL,
 	Users_UserID INT NOT NULL,
 	PRIMARY KEY(Events_EventID, Users_UserID),
-	CONSTRAINT fkUser_Events_User
-	FOREIGN KEY(Users_UserID)
-	REFERENCES Users(UserID),
-	CONSTRAINT fkUser_User_Events
-	FOREIGN KEY(Events_EventID)
-	REFERENCES Events(EventID)
+	FOREIGN KEY(Users_UserID) REFERENCES Users(UserID),
+    FOREIGN KEY(Events_EventID) REFERENCES Events(EventID)
 );
 ALTER TABLE Users_Events AUTO_INCREMENT = 1;
 
@@ -85,3 +62,14 @@ CREATE TABLE IF NOT EXISTS Managers(
 	PRIMARY KEY(ManagerID)
 );
 ALTER TABLE Managers AUTO_INCREMENT = 1;
+
+INSERT INTO Managers(ManagerName, ManagerPassword)
+VALUES ("Marbel", "Picha");
+INSERT INTO Managers(ManagerName, ManagerPassword)
+VALUES ("Rushell", "Zavalaga");
+INSERT INTO Managers(ManagerName, ManagerPassword)
+VALUES ("Erick", "Hualla");
+INSERT INTO Managers(ManagerName, ManagerPassword)
+VALUES ("Eduardo", "Rojas");
+INSERT INTO Managers(ManagerName, ManagerPassword)
+VALUES ("Ricardo", "Rodriguez");
