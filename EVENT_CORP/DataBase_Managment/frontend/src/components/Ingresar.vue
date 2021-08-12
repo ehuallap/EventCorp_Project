a9b1bd5175209b98e311450fe8d3d242f22d8ab2
ehuallap <ehuallap@unsa.edu.pe>
2021-08-12 09:51:02 -0500

    TOKEN DE ACCESO

--- EVENT_CORP/DataBase_Managment/frontend/src/components/Ingresar.vue
+++ EVENT_CORP/DataBase_Managment/frontend/src/components/Ingresar.vue
@@ -17,17 +17,17 @@
                         <div class="col-md-12">
                             <h2>Dirección de correo electrónico: </h2>
                             <div class="form-group">
-                                <input type=text class="form-control" id="Email" placeholder="Dirección de corro electrónico"><br>
+                                <input type=text class="form-control" id="Email" placeholder="Dirección de corro electrónico" v-model="usuarioNuevo.email"><br>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <h2>Contraseña:</h2>
                             <div class="form-group">
-                                <input type=text class="form-control" id="Contraseña" placeholder="Contraseña">
+                                <input type=text class="form-control" id="Contraseña" placeholder="Contraseña" v-model="usuarioNuevo.password">
                             </div>
                         </div>
                         <div class="col-md-12">
-                            <br><a href="#" class="btn btn-primary">Ingresar</a>
+                            <button type="button" class="btn btn-primary" @click="ingresar">Ingresar</button>
                         </div>
                     </div>
                 </div>
@@ -38,7 +38,38 @@
 
 <script>
 export default {
-  name: "Ingresar"
+  name: "Ingresar",
+  data() {
+    return {
+      usuarioNuevo: {
+        password: "",
+        email: ""
+      },
+      token: {}
+    }
+  },
+  methods: {
+    async ingresar() {
+          // Opciones por defecto estan marcadas con un *
+          this.token = await fetch('http://127.0.0.1:5000/authorize', {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Content-Type': 'application/json'
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify({
+              password: this.usuarioNuevo['password'].toString()
+            })
+            })
+          const array = await this.token.json()
+          console.log(array['token']);
+        }
+  }
 }
 </script>
 
